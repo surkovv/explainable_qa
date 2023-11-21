719916090981f61f09e1fe1e62c61792582c0809
slavasurkov <surokpro2@gmail.com>
2023-11-21 20:22:15 +0300

    rm working files

--- custom_graph_kbqa/test.py
+++
@@ -1,7 +0,0 @@
-from deeppavlov import build_model
-
-question = 'what number of religions is chrismation practiced by?'
-# sparql = g([sexpr])
-
-model = build_model('kbqa_custom_graph.json', download=True)
-print(model([question]))
